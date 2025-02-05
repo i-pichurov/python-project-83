673e8a5778fb2c27e5ad81892747cbe47e589f6c
i-pichurov <deadbassein@gmail.com>
2025-02-05 23:43:08 +0300

    first commit

---
+++ hexlet-code/page_analyzer/app.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+from flask import Flask
+
+# Загружаем переменные окружения из .env файла
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+@app.route('/')
+def home_page():
+    return "Hello, World!"
