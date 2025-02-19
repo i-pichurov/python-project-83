990a65a9c77f2290bae1f58fa11e7794c651d1ef
i-pichurov <deadbassein@gmail.com>
2025-02-19 20:59:09 +0300

    build.sh_v2

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ poetry install
 
 # Запускаем команду make install и создаем базу данных
 echo "Запускаем make install..."
-make install && psql -a -d urls_db $DATABASE_URL -f database.sql
+make install && psql -a -d urls_db_35d3 $DATABASE_URL -f database.sql
