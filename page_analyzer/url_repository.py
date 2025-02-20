d9728c957991621423f2f14bbe21b18d0f1f833d
i-pichurov <deadbassein@gmail.com>
2025-02-20 23:04:16 +0300

    make urls_check

--- page_analyzer/url_repository.py
+++ page_analyzer/url_repository.py
@@ -20,14 +20,19 @@ class UrlRepository:
 
 
     def create(self, url_data):
-        with self.conn.cursor() as cur:
-            cur.execute(
-                "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id",
-                (url_data['name'], url_data['created_at'])
-            )
-            url_data['id'] = cur.fetchone()[0]
-        self.conn.commit()
-        return url_data['id']
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id",
+                    (url_data['name'], url_data['created_at'])
+                )
+                url_data['id'] = cur.fetchone()[0]
+            self.conn.commit()
+            return url_data['id']
+
+        except Error as e:
+            print(f"Database error: {e}")  # Логирование ошибки
+            raise
 
 
     def check_by_name(self, url_data):
@@ -51,6 +56,32 @@ class UrlRepository:
                 result = cur.fetchone()
             return result
 
+        except Error as e:
+            print(f"Database error: {e}")  # Логирование ошибки
+            raise
+
+
+    def create_url_check(self, url_check):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO url_checks (url_id, status_code, h1, title, description, created_at) VALUES (%s, %s, %s, %s, %s, %s)",
+                    (url_check['url_id'], url_check['status_code'], url_check['h1'], url_check['title'], url_check['description'], url_check['created_at'])
+                )
+            self.conn.commit()
+
+        except Error as e:
+            print(f"Database error: {e}")  # Логирование ошибки
+            raise
+
+
+    def get_url_checks(self, url_id):
+        try:
+            with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+                cur.execute("SELECT * FROM url_checks WHERE url_id = %s ORDER BY id DESC", (url_id,))
+                result = cur.fetchall()
+                return result
+            
         except Error as e:
             print(f"Database error: {e}")  # Логирование ошибки
             raise
